e1bad610aab8a8390d3325f2ed34facad614614b
atharvkochar <72980492+atharvkochar@users.noreply.github.com>
2021-02-01 17:29:04 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,46 @@
+class Form{
+
+    constructor(){
+        this.input=createInput("Name");
+        this.button - createButton('Play');
+        this.greeting= createElement('h2');
+    }
+
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+    }
+    display(){
+
+        var title = createElement('h2')
+        title.html("Car Racing Game");
+        title.position(130,0);
+
+        
+        input.position(130,160);
+        button.position(250,200);
+
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+            player.name=this.input.value();
+
+
+            playerCount+=1;
+            player.index=playerCount;
+            player.update();
+            player.upadteCount(playerCount);
+
+            this.greeting.html("Hello  " +player.name)
+            this.greeting.position(130,160)
+
+        });
+
+
+
+
+    }
+
+
+}
---
+++ Player.js
@@ -0,0 +1,42 @@
+class Player{
+    constructor(){}
+
+    getCount(){
+var playerCountRef = database.ref('playerCount');
+playerCountRef.on("value",function(data){
+
+    playerCount = data.val();
+
+});
+
+
+    }
+
+    updateCount(count){
+
+        database.ref('/').update({
+            playerCount: count
+
+
+        });
+    }
+
+    update(){
+
+        var playerIndex = "players/player" + this.index;
+
+        database.ref(playerIndex).set({
+            name:this.name,
+            distance:this.distance
+
+        });
+    }
+
+    static gatPlayerInfo(){
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value",(data)=>{
+            allPlayers = data.val();
+
+        })
+    }
+}
